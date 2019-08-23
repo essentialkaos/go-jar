a655a067ed30ea55f69c76744ae0cca747f7712a
Anton Novojilov <andy@essentialkaos.com>
2019-08-23 14:36:27 +0300

    Added usage examples

---
+++ examples_test.go
@@ -0,0 +1,23 @@
+package jar
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                     Copyright (c) 2009-2019 ESSENTIAL KAOS                         //
+//        Essential Kaos Open Source License <https://essentialkaos.com/ekol>         //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"fmt"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func Example_ReadFile() {
+	manifest, err := ReadFile("wagon-file-3.3.2.jar")
+
+	fmt.Printf("Error: %v\n", err)
+	fmt.Printf("Specification-Vendor: %s\n", manifest["Specification-Vendor"])
+	fmt.Printf("Implementation-Vendor-Id: %s\n", manifest["Implementation-Vendor-Id"])
+	fmt.Printf("Specification-Version: %s\n", manifest["Specification-Version"])
+}
